337260dbf224664540c648b6b0f1bfa3d6fa1fb3
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-04 14:46:57 -0400

    Finished first iteration of LongTermGoalsModel

--- Models/LongTermGoalsModel.cs
+++ Models/LongTermGoalsModel.cs
@@ -4,7 +4,11 @@ namespace Capstone_0.Models
 {
   public class LongTermGoalsModel
   {
+    public string OneYearGoals { get; set; }
+    public string ThreeYearGoals { get; set; }
+    public string TenYearGoals { get; set; }
     public string LongTermGoals { get; set; }
+    public string MyHighestSelfICanImagineIs { get; set; }
     public int ID { get; set; }
     public int UserID { get; set; }
     public DateTime AccountCreated { get; set; } = DateTime.Now;
