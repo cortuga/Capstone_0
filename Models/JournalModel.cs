a945b04902cf401aca64c1e73d0aecf4af0bc56e
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-04 14:41:00 -0400

    Journal Model completed

--- Models/JournalModel.cs
+++ Models/JournalModel.cs
@@ -5,7 +5,22 @@ namespace Capstone_0.Models
   public class JournalModel
   {
     //   Still need to add individual questions for Journal. See HPP. 
-    public string JournalEntry { get; set; }
+    public String ExcitedAboutToday { get; set; }
+    public String OneWordToDescribeMeToday { get; set; }
+    public String WhoNeedsMeOnMyAGame { get; set; }
+    public String WhatMightStressMeOut { get; set; }
+    public String TheWayMyBestSelfWouldDealIs { get; set; }
+    public String SomeoneToSurpriseWithAppreciation { get; set; }
+    public String OneActionToDeminstrateExecellence { get; set; }
+    public String OneBoldActionToTakeToday { get; set; }
+    public String LookingAtMyLifeMetaIWouldTellMyself { get; set; }
+    public String ProjectsToKeepInMindForFutureAre { get; set; }
+    public String IWouldKnowTodayIsASuccessIf { get; set; }
+    // public string JournalEntry { get; set; }
+    public String TodaysMessageToMyself { get; set; }
+    public String TodaysTopPriorities { get; set; }
+    public String TasksThatMustBeDoneToday { get; set; }
+    public String PersonsToConnectWithToday { get; set; }
     public int ID { get; set; }
     public int UserID { get; set; }
     public DateTime AccountCreated { get; set; } = DateTime.Now;
