9921e239506a5aea3734c2655afe5dec3eb77c89
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-07 14:08:16 -0400

    working to figure out how to display other components

--- ClientApp/src/components/Journal.js
+++ ClientApp/src/components/Journal.js
@@ -3,35 +3,35 @@ import React from "react"
 const Journal = () => {
   function handleSubmit(e) {
     e.preventDefault() //Considering I want this to take user to Journal history or take user to todo page, i shouldn't prevent default.
-
-    return (
-      <>
-        <div>
-          <form onSubmit={handleSubmit}>
-            <section>
-              <label>
-                Name:
-                <input type='text' name='name' />
-              </label>
-              <input type='submit' value='Submit' />
-              {/* whats going on above?? */}
-
-              <textarea rows='35' cols='80'>
-                Testing This text area
-              </textarea>
-            </section>
-            <section>
-              <label>
-                <h3>Already have a journal entry written?</h3>
-                <h4>Upload that file</h4>
-                <input type='file'></input>
-              </label>
-            </section>
-          </form>
-        </div>
-      </>
-    )
   }
 
-  export default Journal
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <section>
+            <label>
+              Name:
+              <input type='text' name='name' />
+            </label>
+            <input type='submit' value='Submit' />
+            {/* whats going on above?? */}
+
+            <textarea rows='35' cols='80'>
+              Testing This text area
+            </textarea>
+          </section>
+          <section>
+            <label>
+              <h3>Already have a journal entry written?</h3>
+              <h4>Upload that file</h4>
+              <input type='file'></input>
+            </label>
+          </section>
+        </form>
+      </div>
+    </>
+  )
 }
+
+export default Journal
