9a02c337662eabe7d93deaf15b71ea10fa4aa28c
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-07 11:49:12 -0400

    app.js really fixed now

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -14,42 +14,25 @@ import LandingPage from "./components/LandingPage"
 export default class App extends Component {
   static displayName = App.name
 
-  state = {
-    //   todos: [
-    //     {
-    //       id: Math.random() + 1,
-    //       title: "Take out the trash",
-    //       completed: false
-    //     },
-    //     {
-    //       id: 2,
-    //       title: "Dinner",
-    //       completed: false
-    //     },
-    //     {
-    //       id: 3,
-    //       title: "Meeting with boss",
-    //       completed: true
-    //     }
-    //   ]
-    // }
+  // state = {
 
-    render() {
-      return (
-        <>
-          <Router>
-            <Nav />
-            <Switch>
-              <Route exact path='/' component={LandingPage} />
-              <Route path='/Journal' component={Journal} />
-              <Route path='/Todos' component={Todos} />
-              <Route path='/Calender' component={Calender} />
-              <Route path='/LifeGoals' component={LifeGoals} />
-              <Route path='/Profile' component={Profile} />
-            </Switch>
-          </Router>
-        </>
-      )
-    }
+  //    }
+
+  render() {
+    return (
+      <>
+        <Router>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            <Route path='/Journal' component={Journal} />
+            <Route path='/Todos' component={Todos} />
+            <Route path='/Calender' component={Calender} />
+            <Route path='/LifeGoals' component={LifeGoals} />
+            <Route path='/Profile' component={Profile} />
+          </Switch>
+        </Router>
+      </>
+    )
   }
 }
