006b28dd08a0c71a32608b3c5a5d7244d571cc57
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-07 13:10:20 -0400

    testing text area

---
+++ ClientApp/src/components/Journal.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const Journal = () => {
+  return (
+    <>
+      <div>
+        <form>
+          <label>
+            Name:
+            <input type='text' name='name' />
+          </label>
+          <input type='submit' value='Submit' />
+          {/* whats going on above?? */}
+
+          <textarea rows='35' cols='80'>
+            Testing This text area
+          </textarea>
+        </form>
+      </div>
+    </>
+  )
+}
+
+export default Journal
--- ClientApp/src/components/LifeGoals.js
+++ ClientApp/src/components/LifeGoals.js
@@ -1,7 +1,11 @@
 import React from "react"
 
 const LifeGoals = () => {
-  return <div></div>
+  return (
+    <>
+      <div></div>
+    </>
+  )
 }
 
 export default LifeGoals
