5c1a89137c5cc8fabf705e703ea8474b98a7d943
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-09 11:17:02 -0400

    Fixing caps in Journal

--- ClientApp/src/components/Journal.js
+++ ClientApp/src/components/Journal.js
@@ -3,7 +3,18 @@ import axios from "axios"
 
 const Journal = () => {
   const [excited, setExcited] = useState("")
-  const []
+  const [oneWord, setOneWord] = useState("") //not set up
+  const [needsMe, setNeedsMe] = useState("")
+  const [stressMeOut, setStressMeOut] = useState("")
+  const [bestSelf, setBestSelf] = useState("")
+  const [someoneToSurprise, setSomeoneToSurprise] = useState("")
+  const [excellence, setExcellence] = useState("")
+  const [boldAction, setBoldAction] = useState("")
+  const [lifeMeta, setLifeMeta] = useState("")
+  const [futureProjects, setFutureProjects] = useState("")
+  const [successfulToday, setSuccessfulToday] = useState("")
+  const [topPriorities, setTopPriorities] = useState("")
+  const [mustBeDoneToday, setMustBeDoneToday] = useState("")
 
   const handleSubmit = e => {
     e.preventDefault() //Considering I want this to take user to Journal history or take user to todo page, i shouldn't prevent default.
@@ -16,14 +27,14 @@ const Journal = () => {
         TheWayMyBestSelfWouldDealIs: bestSelf,
         SomeoneToSurpriseWithAppreciation: someoneToSurprise,
         OneActionToDemonstrateExcellence: excellence,
-        OneBoldActionToTakeToday: BoldAction,
+        OneBoldActionToTakeToday: boldAction,
         LookingAtMyLifeMetaIWouldTellMyself: lifeMeta,
         ProjectsToKeepInMindForFutureAre: futureProjects,
         IWouldKnowTodayIsASuccessIf: successfulToday,
         TodaysMessageToMyself: todaysMessage,
         TodaysTopPriorities: topPriorities,
         TasksThatMustBeDoneToday: mustBeDoneToday,
-        PersonsToConnectWithToday: connectToday,
+        PersonsToConnectWithToday: connectToday
       })
       console.log(resp)
     }
@@ -40,13 +51,13 @@ const Journal = () => {
                 Today's Message to Myself:
               </textarea>
             </label>
-            {/* what is this label looking for? */}
 
             {/* the question above doesn't have a place in the model. Going to make the other 3 ree pillers part of the todo section */}
             {/* <textarea rows='5' cols='50'>
               Today's Top 3 Goals / Priorities...
             </textarea> */}
             {/* Want to make the Top 3 goals passed to the Todos component */}
+
             <textarea
               rows='5'
               cols='50'
@@ -56,42 +67,131 @@ const Journal = () => {
                 ? excited
                 : "One Thing I can get excited about today is..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              If one word could describe the kind of person I want to be today,
-              then that word is... and why I chose it...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onChange={e => setOneWord(e.target.value)}
+            >
+              {oneWord
+                ? oneWord
+                : " If one word could describe the kind of person I want to be today, then that word is... and why I chose it..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              Someone who need me on my A game today is...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onChange={e => setNeedsMe(e.target.value)}
+            >
+              {needsMe
+                ? needsMe
+                : "Someone who need me on my A game today is..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              A situation that might stress me out or trip me up today...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onChange={e => setStressMeOut(e.target.value)}
+            >
+              {stressMeOut
+                ? stressMeOut
+                : "A situation that might stress me out or trip me up today..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              ...and the way that my best self would deal with this is...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onChange={e => setBestSelf(e.target.value)}
+            >
+              {bestSelf
+                ? bestSelf
+                : "...and the way that my best self would deal with this is..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              Someone I could surprise with a note, gift,or sign of appreciation
-              is...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onChange={e => setSomeoneToSurprise(e.target.value)}
+            >
+              {someoneToSurprise
+                ? someoneToSurprise
+                : "Someone I could surprise with a note, gift,or sign of appreciation is..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              One action I could take today to demonstrate excellence or real
-              value is...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onChange={e => setExcellence(e.target.value)}
+            >
+              {excellence
+                ? excellence
+                : "One action I could take today to demonstrate excellence or real value is..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              One bold action I could take today is...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onClick={e => setBoldAction(e.target.value)}
+            >
+              {boldAction
+                ? boldAction
+                : "One bold action I could take today is..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              If I highest self was looking at my life from a high level, I
-              would tell myself to remember that...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onClick={e => setLifeMeta(e.target.value)}
+            >
+              {lifeMeta
+                ? lifeMeta
+                : "If I highest self was looking at my life from a high level, I would tell myself that..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              The big projects I have to keep in mind that I want to take on,
-              even if I can't act toward them today, are...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onClick={e => setFutureProjects(e.target.value)}
+            >
+              {futureProjects
+                ? futureProjects
+                : "The big projects I have to keep in mind that I want to take on, even if I can't act toward them today, are..."}
             </textarea>
-            <textarea rows='5' cols='50'>
-              I would know that today was a great success if at the end of the
-              day I did or felt these things...
+
+            <textarea
+              rows='5'
+              cols='50'
+              onClick={e => setSuccessfulToday(e.target.value)}
+            >
+              {successfulToday
+                ? successfulToday
+                : "I would know that today was a great success if at the end of the day I did or felt these things"}
             </textarea>
+
+            <textarea
+              rows='5'
+              cols='50'
+              onClick={e => setTopPriorities(e.target.value)}
+            >
+              {topPriorities
+                ? topPriorities
+                : "My top 3 priorities or goals today are..."}
+            </textarea>
+
+            <textarea
+              rows='5'
+              cols='50'
+              onClick={e => setMustBeDone(e.target.value)}
+            >
+              {mustBeDoneToday
+                ? mustBeDoneToday
+                : "Tasks that must be done today"}
+            </textarea>
+
+            <textarea rows='5' cols='50' onClick={e => set(e.target.value)}>
+              {replace ? replace : "persons to connect with"}
+            </textarea>
+
             <input type='submit' value='Submit' />
           </section>
 
