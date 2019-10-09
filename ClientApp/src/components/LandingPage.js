878e9f4f7af844a3f0dd35b62ea296e6ff8a5a4f
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-08 23:11:16 -0400

    Finished Questions. Error currently

--- ClientApp/src/components/LandingPage.js
+++ ClientApp/src/components/LandingPage.js
@@ -7,23 +7,23 @@ const LandingPage = () => {
 
       <h2>Defining your day and your future:</h2>
       <p>
-        I define high performance as reaching and sustaining long-term success,
-        while also maintaining well being and positive relationships. That last
-        part is important. People often assume you have to compromise your
-        health or relationships in order to succeed at high levels but the
-        research doesn't support this. With the right life habits and daily
-        pracitices, you can achieve your dreams, out perform your yesterdays and
-        grow faster then you ever imagined, and strive with true joy and
-        fulfillment. Of course everyone wants to be successful. But few people
-        create the right mindset, set real priorities, define what's meaningful
-        to them, and review their day and performance in measurable ways that
-        can propel their growth. I've found that most prople don't ever know the
-        basic planning and prioritization strageties that lead to long-term
-        success. That's why I created The Compass. I wanted you to have a proven
-        and strategic approach to developing yourself and achieving your goals.
-        I wanted to help instill thinking patterns and practices of some of the
-        most high performing individuals in the world today and bring some of
-        that to your daily life.
+        I define high performance as reaching and personal goals with a balanced
+        life, maintaining well being and positive relationships. That last part
+        is important. People often assume you have to compromise your health or
+        relationships in order to succeed but the research doesn't support this.
+        With the right life habits and daily pracitices, you can achieve your
+        dreams, out perform your yesterdays and grow faster then you ever
+        imagined, and strive with true joy and fulfillment. Of course everyone
+        wants to be successful. But few people create the right mindset, set
+        real priorities, define what's meaningful to them, and review their day
+        and performance in measurable ways that can propel their growth. I've
+        found that so many people don't ever work on the basic planning and
+        prioritization strageties that so often lead to long-term success or
+        self cultivation. That's why I created The Compass. I wanted you to have
+        a proven and strategic approach to developing yourself and achieving
+        your goals. I wanted to help instill thinking patterns and practices of
+        some of the most high performing individuals in the world today and
+        bring some of that to your daily life.
       </p>
     </div>
   )
