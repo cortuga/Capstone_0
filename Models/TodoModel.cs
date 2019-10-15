f2ed959ae73ccbbf823d478a34ad318dc541963d
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-15 14:37:16 -0400

    Commented out user model reqs

--- Models/TodoModel.cs
+++ Models/TodoModel.cs
@@ -8,8 +8,8 @@ namespace Capstone_0.Models
     // what data type would be a to do list? array / list? What is the common ansester for these types? IEnumerable?
     public string Text { get; set; }
     public int ID { get; set; }
-    public int UserProfileModelID { get; set; }
-    public UserProfileModel UserProfileModel { get; set; }
+    // public int UserProfileModelID { get; set; }
+    // public UserProfileModel UserProfileModel { get; set; }
 
 
     public DateTime AccountCreated { get; set; } = DateTime.Now;
