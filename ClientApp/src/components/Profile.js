7661ecfa2e827339c790d7faa4e6f398a69b9e35
Corey Grandmaison <cgsustainability@gmail.com>
2019-10-16 15:23:32 -0400

    Profile is displaying user data

--- ClientApp/src/components/Profile.js
+++ ClientApp/src/components/Profile.js
@@ -3,19 +3,14 @@ import axios from "axios"
 
 const Profile = () => {
   const [username, setUsername] = useState([])
-  const [password, setPassword] = useState("")
-  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState([])
+  const [email, setEmail] = useState([])
   const [accountCreated, setAccountCreated] = useState("")
-  const [dream, setDream] = useState("")
+  const [dream, setDream] = useState([])
 
   const GetProfileRequest = async () => {
-    const resp = await axios.get("https://localhost:5001/api/UserProfile", {
-      Username: username,
-      Password: password,
-      Email: email,
-      AccountCreated: accountCreated
-    })
-    console.log(resp.data)
+    console.log("data")
+    const resp = await axios.get("https://localhost:5001/api/UserProfile/1")
     // const data = resp.data
     setUsername(resp.data.username)
     setPassword(resp.data.password)
@@ -27,7 +22,7 @@ const Profile = () => {
     const resp = await axios.get("https://localhost:5001/api/LongTermGoals")
 
     console.log(resp.data)
-    // setDream(resp.data[0])
+    setDream(resp.data)
   }
 
   useEffect(() => {
@@ -39,11 +34,17 @@ const Profile = () => {
     <>
       <div class='container'>
         <p class='subtitle'>Profile page</p>
-        <h1 class='text'>Username:{username}</h1>
-        <h2 class='password'>Password:{password}</h2>
+
+        <h1 class='text'>Username:</h1>
+        <h1>{username}</h1>
+        <section class='container'>
+          <h2 class='password'>Password:</h2>
+          <h2>{password}</h2>
+        </section>
+
         <h2>Email: {email}</h2>
         <h2>Date Created: {accountCreated}</h2>
-        <h2>LGT / Dreams: {dream}</h2>
+        {/* <h2>LTG / Dreams: {dream}</h2> */}
 
         <div class='has-text-centered section container'>
           <footer>Made with 💛 at SDG</footer>
